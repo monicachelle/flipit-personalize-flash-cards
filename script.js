44faecb8dab793e78c6990a6aa40b3787ca7cc49
monicachelle <monicachelle18@gmail.com>
2026-03-10 08:45:01 +0530

    Fix syntax error in validUsers array

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const validUsers = [
   { username: "user", password: "user123" },
   { username: "student", password: "learn123" },
   { username: "demo", password: "demo123" },
-  { username: "Monica Raghini", password: "moni@26" }
-   { username: "1234", password: "1234" }
+  { username: "Monica Raghini", password: "moni@26" },
+  { username: "1234", password: "1234" }
   
 ];
 
